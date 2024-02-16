3538ccae0725fe9e46dcabd7311399e2918bc179
millemaliu <millemal@iu.edu>
2024-02-15 19:40:44 -0500

    Update index.js

--- index.js
+++ index.js
@@ -1,7 +1,7 @@
 
 
 
-async function newAnswer() {
+async function add() {
     const a = document.getElementById("first");
     const b = document.getElementById("second");
     const x = document.getElementById("answer");
@@ -9,6 +9,51 @@ async function newAnswer() {
     let second = parseInt(b.value);
     
     const response = await fetch("https://arithmetic-service-68hv.onrender.com/add/" + first + "/" + second);
+    const sum = await response.json();
+    
+    
+    x.innerHTML = sum;
+}
+
+
+async function subtract() {
+    const a = document.getElementById("first-");
+    const b = document.getElementById("second-");
+    const x = document.getElementById("answer-");
+    let first = parseInt(a.value);
+    let second = parseInt(b.value);
+    
+    const response = await fetch("https://arithmetic-service-68hv.onrender.com/subtract/" + first + "/" + second);
+    const sum = await response.json();
+    
+    
+    x.innerHTML = sum;
+}
+
+
+async function divide() {
+    const a = document.getElementById("firstd");
+    const b = document.getElementById("secondd");
+    const x = document.getElementById("answerd");
+    let first = parseInt(a.value);
+    let second = parseInt(b.value);
+    
+    const response = await fetch("https://arithmetic-service-68hv.onrender.com/divide/" + first + "/" + second);
+    const sum = await response.json();
+    
+    
+    x.innerHTML = sum;
+}
+
+
+async function multiply() {
+    const a = document.getElementById("first*");
+    const b = document.getElementById("second*");
+    const x = document.getElementById("answer*");
+    let first = parseInt(a.value);
+    let second = parseInt(b.value);
+    
+    const response = await fetch("https://arithmetic-service-68hv.onrender.com/multiply/" + first + "/" + second);
     const sum = await response.json();
     
     
